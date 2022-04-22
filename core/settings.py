2a90a1ef1838c853bd35e41e660f53fc284e177c
ashraf alroomi <ashrafalroomi5@gmail.com>
2022-04-22 06:22:05 +0300

    init

--- core/settings.py
+++ core/settings.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
-API_BASE_URL = "https://sandbox-api.coinmarketcap.com/v1/"
-API_KEY = "14a27bdb-20a2-4be1-9a8a-c54aa17431cb"
+API_BASE_URL = ""
+API_KEY = ""
 
 
 class EndPoints(Enum):
